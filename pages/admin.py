c26d51ccbaa58e79255dd9ad29e85c199d991af4
ANSHIKA <160839566+ANSHIka132004@users.noreply.github.com>
2025-12-15 14:44:37 +0530

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -2,13 +2,14 @@ import streamlit as st
 import pandas as pd
 import os
 
-st.title("Admin Dashboard")
-
 FILE_NAME = "submissions.csv"
+
+st.title("Admin Dashboard")
 
 if not os.path.exists(FILE_NAME):
     st.warning("No submissions yet.")
 else:
-    df = pd.read_csv(FILE_NAME)
+    # Python engine handles inconsistent columns safely
+    df = pd.read_csv(FILE_NAME, engine="python")
     st.dataframe(df)
 
