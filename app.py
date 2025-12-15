16bbc62b8a010214748056decbccb586b8873645
ANSHIKA <160839566+ANSHIka132004@users.noreply.github.com>
2025-12-15 14:15:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.title("AI Feedback System")
-st.write("Task 2 is running successfully.")
+st.title("Leave a Review")
+
+rating = st.selectbox("Select rating", [1,2,3,4,5])
+review = st.text_area("Write your review")
+
+if st.button("Submit"):
+    st.success("Thanks for your feedback!")
+    st.write("Rating:", rating)
+    st.write("Review:", review)
