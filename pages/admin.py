9253e298f145db75ef45c7795886944f915168d6
ANSHIKA <160839566+ANSHIka132004@users.noreply.github.com>
2025-12-15 14:30:09 +0530

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import os
+
+st.title("Admin Dashboard")
+
+FILE_NAME = "submissions.csv"
+
+if not os.path.exists(FILE_NAME):
+    st.warning("No submissions yet.")
+else:
+    df = pd.read_csv(FILE_NAME)
+    st.dataframe(df)
 
